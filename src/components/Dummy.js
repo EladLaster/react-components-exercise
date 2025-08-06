7eb54a90cd7509450324c66c976602367c601936
EladLaster <eladlaster1@walla.com>
2025-08-06 12:46:57 +0300

    Done component ex

--- src/components/Dummy.js
+++ src/components/Dummy.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
-const Dummy = () => {}
+const Dummy = () => {
+    return <div>
+        <input type="text" placeholder="Type here..." />
+        <button>Click me</button>
+    </div> 
+    
+}
 
 export default Dummy
--- src/components/Spamalot.js
+++ src/components/Spamalot.js
@@ -2,7 +2,12 @@ import Spam from "./Spam";
 import React, { Component } from 'react';
 
 
-const Spamalot =()=>{}
+const Spamalot =()=>{
+    const spams = []
+    for(let i = 0 ;i<500;i++)
+        spams.push(<Spam key={i} />);
+    return spams;
+}
 
 export default Spamalot
 
